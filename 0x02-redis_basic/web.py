2c0c97176632f8e7872f39be7a32d07227f216de
francisamanyo <amanyofrancis@yahoo.com>
2023-10-25 10:05:52 -0700

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,38 @@
 #!/usr/bin/env python3
-"""Expiring Web Cache Module"""
-
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
-from typing import Callable
 from functools import wraps
-
-# Initialize a Redis client
-redis = redis.Redis()
+from typing import Callable
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """
-    Decorator to cache web responses and track URL access counts
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
-    :param fn: The function to be wrapped
-    :return: Wrapped function with caching and access count tracking
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper for the decorator to cache web
-        responses and track URL access counts
 
-        :param url: The URL to retrieve the HTML content from
-        :return: The HTML content of the URL
-        """
-        # Increment the access count for the URL
-        redis.incr(f"count:{url}")
-
-        # Check if the response is cached in Redis
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-
-        # If not cached, call the original function and cache the response
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
+    return invoker
 
-    return wrapper
 
-
-@wrap_requests
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Fetch a web page content from a given URL and cache the result
-
-    :param url: The URL to retrieve the HTML content from
-    :return: The HTML content of the URL
-    """
-    response = requests.get(url)
-    return response.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
