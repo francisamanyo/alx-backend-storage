047e43b96d4ce5d3778eb41c7bedccdb14f74f9c
francisamanyo <amanyofrancis@yahoo.com>
2023-10-25 09:46:35 -0700

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,54 @@
 #!/usr/bin/env python3
-""" Redis Module """
+"""Expiring Web Cache Module"""
 
-from functools import wraps
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
-redis_ = redis.Redis()
+# Initialize a Redis client
+redis = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decortator for counting """
-    @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+def wrap_requests(fn: Callable) -> Callable:
+    """
+    Decorator to cache web responses and track URL access counts
+
+    :param fn: The function to be wrapped
+    :return: Wrapped function with caching and access count tracking
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """
+        Wrapper for the decorator to cache web
+        responses and track URL access counts
+
+        :param url: The URL to retrieve the HTML content from
+        :return: The HTML content of the URL
+        """
+        # Increment the access count for the URL
+        redis.incr(f"count:{url}")
+
+        # Check if the response is cached in Redis
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+
+        # If not cached, call the original function and cache the response
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
 
     return wrapper
 
 
-@count_requests
+@wrap_requests
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    req = requests.get(url)
-    return req.text
+    """
+    Fetch a web page content from a given URL and cache the result
+
+    :param url: The URL to retrieve the HTML content from
+    :return: The HTML content of the URL
+    """
+    response = requests.get(url)
+    return response.text
